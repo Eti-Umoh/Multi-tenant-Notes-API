f755a0a2c556f3218d8d633a490d935b1773eb86
Eti Umoh <etiumoh04@gmail.com>
2025-10-27 12:39:19 +0100

    config and db

--- server/config.py
+++ server/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     SECRET: str
     ALGORITHM: str
     MONGO_URL: str
+    DB_NAME: str
     class Config:
         env_file = "server/.env"
 
--- server/db.py
+++ server/db.py
@@ -2,4 +2,4 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from server.config import settings
 
 client = AsyncIOMotorClient(settings.MONGO_URL)
-db = client["notes_db"]
+db = client[settings.DB_NAME]
