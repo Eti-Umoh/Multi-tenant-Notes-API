eeaf4acf69fc92ad29361361665263b0a1c94c70
Eti Umoh <etiumoh04@gmail.com>
2025-10-27 13:25:04 +0100

    note models

---
+++ server/notes/models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+from datetime import datetime, timezone
+from server.common import PyObjectId
+
+class NoteBase(BaseModel):
+    title: str
+    content: str
+    organization_id: PyObjectId
+    created_by: PyObjectId  # user id
+
+class NoteCreate(NoteBase):
+    pass
+
+class NoteInDB(NoteBase):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    class Config:
+        populate_by_name = True
+        json_encoders = {PyObjectId: str}
