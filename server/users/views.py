9c78f93631e8e7e78aeaadd6cdf719c81221afae
Eti Umoh <etiumoh04@gmail.com>
2025-10-27 16:15:58 +0100

    remove

--- server/users/views.py
+++ server/users/views.py
@@ -5,10 +5,7 @@ from server.main_utils import (resource_conflict_response, created_response,
                                resource_not_found_response, un_authenticated_response,
                                un_authorized_response)
 from datetime import datetime, timezone
-from server.authentication.utils import generate_random_password, authorize_jwt_subject
-import bcrypt
-from fastapi.params import Depends
-from bson import ObjectId
+from server.authentication.utils import authorize_jwt_subject
 
 
 router = APIRouter()
