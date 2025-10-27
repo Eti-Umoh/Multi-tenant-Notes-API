2171beb0f99171fe34a2c2456aa02afc99a51392
Eti Umoh <etiumoh04@gmail.com>
2025-10-27 13:11:54 +0100

    org models

--- server/organizations/models.py
+++ server/organizations/models.py
@@ -2,23 +2,19 @@ from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 from bson import ObjectId
 from datetime import datetime
-from server.models.common import PyObjectId  # from our earlier step
+from server.common import PyObjectId  # from our earlier step
 
-class UserBase(BaseModel):
-    first_name: str
-    last_name: str
-    email_address: EmailStr
-    phone_number: str
-    gender: Optional[str] = None
-    user_type: Optional[str] = "investor"
-    referral_code: Optional[str] = None
+class OrgBase(BaseModel):
+    name: str
+    
 
-class UserCreate(UserBase):
-    password: str
+class OrgCreate(OrgBase):
+    pass
 
-class UserInDB(UserBase):
+class OrgInDB(OrgBase):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
     class Config:
         populate_by_name = True
